036e5421ac12a6f8c6a4b298bbf1350cbd37db20
MoDuJli <darkman700@mail.ru>
2019-03-08 11:49:52 +0300

    Заканчиваю проект

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-/*Попап обратной связи на главной*/ 
-
-var link = document.querySelector(".link-modal-feedback");
-var popup = document.querySelector(".modal-feedback");
+var link = document.querySelectorAll(".modal-link");
+var popup = document.querySelector(".modal");
 var popupClose = popup.querySelector(".modal-close");
 var form = popup.querySelector("form");
 var login = popup.querySelector("[name=login]");
@@ -12,31 +10,34 @@ var storage = "";
 
 try {
     storage = localStorage.getItem("login");
-} catch (err) {                                     /*Проверяем работу localStorage */
+} catch (err) {                                  
     isStorageSupport = false;
 }
 
-link.addEventListener("click", function(evt) {  
-    evt.preventDefault();                           /*Открываем попап */
-    popup.classList.add("modal-show");   
-    if (storage) {
-        login.value = storage;
-        email.focus();
-    } else {
-        login.focus();
-    }
-});
+for(var i=0;i<link.length;i++) {    
+    link[i].addEventListener("click", function(evt) {  
+        evt.preventDefault();                          
+        popup.classList.add("modal-show");   
+        if (storage) {
+            login.value = storage;
+            email.focus();
+        } else {
+            login.focus();
+        }
+    })
+};
 
 popupClose.addEventListener("click", function(evt) {
-    evt.preventDefault();                           /*Закрываем попап */
+    evt.preventDefault();                         
     popup.classList.remove("modal-show");
     popup.classList.remove("modal-error"); 
 });
 
+if (form) {
 form.addEventListener("submit", function(evt) {
     if (!login.value || !email.value) {
         evt.preventDefault();
-        popup.classList.remove("modal-error");      /*Проверяем заполнение форм */
+        popup.classList.remove("modal-error");      
         popup.offsetWidth = popup.offsetWidth;
         popup.classList.add("modal-error");
     } else {
@@ -44,16 +45,14 @@ form.addEventListener("submit", function(evt) {
             localStorage.setItem("login", login.value);
         }
     }
-});
+})};
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-        if (popup.classList.contains("modal-show") || map.classList.contains("modal-show") || catalogPopup.classList.contains("modal-show")) {  /*Закрыаем формы через ESC*/
+        if (popup.classList.contains("modal-show")) {  
             evt.preventDefault();
             popup.classList.remove("modal-show");
             popup.classList.remove("modal-error");
-            map.classList.remove("modal-show");
-            catalogPopup.classList.remove("modal-show");
         }
     }
 }); 
@@ -62,6 +61,7 @@ window.addEventListener("keydown", function (evt) {
 
 var mapLink = document.querySelector(".map-link");
 var map = document.querySelector(".modal-map");
+if(map) {
 var mapClose = map.querySelector(".modal-close");
 
 mapLink.addEventListener("click", function(evt) {
@@ -74,18 +74,11 @@ mapClose.addEventListener("click", function(evt) {
     map.classList.remove("modal-show");
 });
 
-/*Попап в каталоге */ 
-
-var catalogPopupLink = document.querySelectorAll(".to-buy");
-var catalogPopup = document.querySelector(".modal-added-in-basket");
-var catalogClose = catalogPopup.querySelector(".modal-close");
-
-catalogPopupLink.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    catalogPopup.classList.add("modal-show");
-})
-
-catalogClose.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    map.classList.remove("modal-show");
-});
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (map.classList.contains("modal-show")) { 
+            evt.preventDefault();
+            map.classList.remove("modal-show");
+        }
+    }
+})}; 
